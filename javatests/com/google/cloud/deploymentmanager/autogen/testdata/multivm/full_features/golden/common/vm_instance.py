ab6e509cfc42558d4d80a3b5cf75054fc91e229f
Cloud Marketplace Tools Robot <cloud-marketplace-tools-robot@google.com>
2020-01-13 02:10:39 +0000

    Updating click to deploy scripts based on Vini's CL CL 282829870. Manually ran the deployments and worked fine for most. For some of them i was getting second warning in the link: https://cloud.google.com/deployment-manager/docs/migrate-to-python3 It was happening in templates which is using password.py as that is using random and that non-determinism is acceptable as per above link.
    PiperOrigin-RevId: 289361672

--- javatests/com/google/cloud/deploymentmanager/autogen/testdata/multivm/full_features/golden/common/vm_instance.py
+++ javatests/com/google/cloud/deploymentmanager/autogen/testdata/multivm/full_features/golden/common/vm_instance.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Creates an Instance VM with common defaults."""
-# pylint: disable=g-import-not-at-top
 import copy
 import re
 import common
