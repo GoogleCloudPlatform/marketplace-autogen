2e9625702ab4d9e56f34cb089573088bef1bbcbf
gibbley <gibbley@google.com>
2024-01-16 14:01:28 -0800

    Add Passwords to MultiVm
    PiperOrigin-RevId: 598950053

--- javatests/com/google/cloud/deploymentmanager/autogen/testdata/multivm/full_features/tf/modules/main/variables.tf
+++ javatests/com/google/cloud/deploymentmanager/autogen/testdata/multivm/full_features/tf/modules/main/variables.tf
@@ -8,6 +8,24 @@ variable "zone" {
   description = "The zone for the solution to be deployed."
 }
 
+variable "admin_password" {
+  type        = string
+  description = "Password for Admin."
+  sensitive   = true
+}
+
+variable "ghost_mysql_password" {
+  type        = string
+  description = "Password for Ghost MySQL."
+  sensitive   = true
+}
+
+variable "this_is_optional_password" {
+  type        = string
+  description = "Password for This is optional."
+  sensitive   = true
+}
+
 variable "instance_count" {
   type        = number
 }
